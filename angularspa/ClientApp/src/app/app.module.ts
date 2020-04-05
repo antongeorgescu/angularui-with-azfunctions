43e52af3cd1b478d74605414f7047ab30a471203
antongeorgescu <antongeorgescu@gmail.com>
2020-04-05 10:55:34 -0400

    Added MSAL code

--- angularspa/ClientApp/src/app/app.module.ts
+++ angularspa/ClientApp/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { TestBlockerComponent } from './test-blocker/test-blocker.component';
 import { CountryInfoComponent } from './country-info/country-info.component';
+import { MsalModule, MsalInterceptor, MsalGuard } from '@azure/msal-angular';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
   declarations: [
@@ -26,15 +29,47 @@ import { CountryInfoComponent } from './country-info/country-info.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    MsalModule.forRoot({
+      auth: {
+        clientId: "b4bd03e4-cb74-40cf-90c1-1a1bfde4b80e",
+        authority: "https://login.microsoftonline.com/e8422127-880e-4288-928e-4ced14423628",
+        redirectUri: "http://localhost:27822"
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: isIE, // set to true for IE 11
+      }
+    },
+    {
+      popUp: !isIE,
+      consentScopes: [
+        'user.read',
+        'openid',
+        'profile',
+      ],
+      unprotectedResources: [],
+      protectedResourceMap: [
+        ['https://graph.microsoft.com/v1.0/me', ['user.read']]
+      ],
+      extraQueryParameters: {}
+    }),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
+      //{ path: 'fetch-data', component: FetchDataComponent, canActivate : [MsalGuard] },
+      //{ path: 'country-info', component: CountryInfoComponent, canActivate: [MsalGuard] },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'country-info', component: CountryInfoComponent },
       { path: 'test-blocker', component: TestBlockerComponent },
     ])
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
